92a1582d3f99fbde5b038ba62d3a261308884f37
Sina Dabiri <sina.dabiri@berkeley.edu>
2021-10-20 22:00:26 -0400

    using nanmedian to avoid NaN issue.

--- QT_measurements.m
+++ QT_measurements.m
@@ -14,30 +14,28 @@ addpath('C:\Users\sinad\OneDrive - Georgia Institute of Technology\CliffordandSa
 data_base_cor_csv = csvread('preprocessed.csv');
 fs0 = '1000';
 % ToDO: specifiy QT_output.csv location folder
-% QT_output = '\QT_pipeline\QT_output.csv'
-QT_analysis('preprocessed.csv', fs0, '14', '14', 'QT_output.csv', 'w');
+QT_output = 'C:\Users\sinad\OneDrive - Georgia Institute of Technology\CliffordandSameni\GitRepos\QT_pipeline\QT_output.csv'
+QT_analysis('preprocessed.csv', fs0, '14', '14', QT_output, 'w');
 
 QT = csvread('QT_output.csv', 0,1);
 QT_reshaped = reshape(QT(1:70), [5, 14]);
 md_QT_Qiao = zeros(1,14);
 channel = 1;
 for ch=1:14
-    md_QT_Qiao(ch) = median(QT_reshaped(:,ch))/fs; % finding the mean of QT values and 
+    md_QT_Qiao(ch) = nanmedian(QT_reshaped(:,ch))/fs; % finding the mean of QT values and 
                                          % normalizing by sampling frequency
-    
 end
 % 
 % [QT1, RR1] = QT_analysis_single_lead(data_base_cor_csv(:,1),fs) 
 % md_QT_Qiao = median(QT1)
-%% 2- Model Based method: Dr. Fattahi
+%% 2- Model Based method: Mr. Fattahi
 % https://github.com/alphanumericslab/OSET/tree/master/UnderDevelopment/QTinterval
 fs = str2double(fs0);
 [GaussParams, rPeaks, soi, waveParams, qtInt]=qtParamsGausFit(data_base_cor_csv, fs);
 md_QT_Fattahi = zeros(1,14);
 for ch=1:14
-    md_QT_Fattahi(ch) = median(qtInt(1,1:165,ch));
+    md_QT_Fattahi(ch) = nanmedian(qtInt(1,:,ch));
 end
-md_QT_Fattahi
 
 %% Plotting Dr. Li vs. Mr. Fattahi QT measurments
 
