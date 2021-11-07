8b401ae8285aa8a2beb516d2f84357fcb7cbf432
Sina Dabiri <sina.dabiri@berkeley.edu>
2021-11-07 17:14:22 -0500

    adding powerline noise filter back in, adding an example image of patient 1 record s0014lre for README.

--- preprocessing.m
+++ preprocessing.m
@@ -26,7 +26,7 @@ function [fs] = preprocessing(inPath, outPath, nChannels)
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
 % Public License for more details.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%
+%% Reading the ECG record using WFDB rdsamp function.
 
 close all;
 
@@ -41,41 +41,23 @@ data = sig'; %Transpose the signal to have as row vector.
 %% Dr. Sameni's OSET baseline removal
 % Using the median method to remove baseline:
 
-% figure()
-% hold on
-% plot(tm, sig, 'b-');
-
 wlen = round(0.6*fs);%set window length as 600 msec
 b_md = BaseLine1(data, wlen,'md');
 
-% plot(tm, b_md, 'r-', 'LineWidth',2);
-% title("The sliding Median filter");
-% hold off
-
 % Using the mean method to remove the baseline:
 
-% figure()
-% hold on
-% plot(tm, sig, 'b-');
-% 'ma' with wlen=0.3 sec
 wlen = round(0.8*fs);
 b_mn = BaseLine1(data, wlen,'mn');
 
-% plot(tm, b_mn, 'g-', 'LineWidth',2);
-% title("The sliding Mean filter");
-% hold off
 
-% figure()
 data_base_cor = data - b_md;
-% plot(tm, data_base_cor, 'b-');
-% title("The channel base-corrected");
+
 
 %% Check to see if Powerline noise is present
 
 % TODO: use periodogram to see if there is a peak at 50 or 60 Hz.
 % add an if statement to remove power noise if there is a peak
 [L, ~] = size(data_base_cor); %length of the signal
-% L = 115200; %length of the signal
 
 data_base_cor_f_two_sided = fft(data_base_cor);
 dbc_two_sided = abs(data_base_cor_f_two_sided/L);
@@ -92,25 +74,25 @@ f = fs*(0:(L/2)) /L;
 % Don't see a peak at 50 or 60 Hz, don't need to do power line noise
 % cancellation
 
-% %% Powerline noise cancellation
-% % notch filter design
-% 
+%% Powerline noise cancellation
+% notch filter design
+
 % fs; % sampling frequency (Hz)
-% f0 = 60; % powerline frequency (50Hz or 60Hz, depending on the country that
-%          % the data has been recorded in)
-% Q = 40; % Q-factor (quality factor) of the notch filter. a parameter that 
-%         % controls the notch quality and is a compromise between transient 
-%         % response time and notch quality
-% Wo = f0/(fs/2); % powerline frequency normalized by the Nyquist frequency 
-% BW = Wo/Q; % normalized notch filter bandwidth
-% [b,a] = iirnotch(Wo,BW); % design the filter numerator and denominator
-%  
-% % looking at the frequency response
-% % fvtool(b, a)
-% x=  data_base_cor;
-% % filtering a 1-D signal x (apply it on each channel if the signal is 
-% % multi-dimensional)
-% y = filter(b, a, x);
+f0 = 60; % powerline frequency (50Hz or 60Hz, depending on the country that
+         % the data has been recorded in)
+Q = 40; % Q-factor (quality factor) of the notch filter. a parameter that 
+        % controls the notch quality and is a compromise between transient 
+        % response time and notch quality
+Wo = f0/(fs/2); % powerline frequency normalized by the Nyquist frequency 
+BW = Wo/Q; % normalized notch filter bandwidth
+[b,a] = iirnotch(Wo,BW); % design the filter numerator and denominator
+ 
+% looking at the frequency response
+% fvtool(b, a)
+x=  data_base_cor;
+% filtering a 1-D signal x (apply it on each channel if the signal is 
+% multi-dimensional)
+y = filter(b, a, x);
 % % figure()
 % % plot(tm, y, 'b-')
 % % title("The channel signal after powerline noise filter");
@@ -118,5 +100,5 @@ f = fs*(0:(L/2)) /L;
 % periodogram(y)
 %% CSV write
 
-csvwrite(outPath, data_base_cor');
+csvwrite(outPath, y');
 end 
